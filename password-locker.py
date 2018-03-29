4fba236d36d8280361322d2cc40977b7d620254c
christinekarimi <karimikim3@gmail.com>
2018-03-29 09:17:39 +0300

    Created a test file for the password-locker

--- password-locker.py
+++ password-locker.py
@@ -1,11 +1,11 @@
 import string
 
 class User:
-  """
-  Class that generates new instances of user
-  """
 
-  user_list = [] #empty user list
+  #This class creates new instances of the user
+  
+
+  user_list = [] #empty list
   def __init__(self, user_name, password,email):
     self.user_name = user_name
     self.password = password
