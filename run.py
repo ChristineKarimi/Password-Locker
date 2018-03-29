0c6080bb199d237eacc2dc46191f2ca227badf9e
christinekarimi <karimikim3@gmail.com>
2018-03-29 11:20:28 +0300

    Modified the run.py file

--- run.py
+++ run.py
@@ -92,3 +92,72 @@ def display_users():
                 print('\n')
         else:
             print("There are no users saved yet")
+
+
+    elif short_code == "ln":
+        print("Please Enter your Username and your Password to log in")
+        print('-' * 30)
+        user_name = input("Username: ")
+        password = input("password: ")
+        sign_in = login_user(user_name,password)
+        if sign_in == True:
+            print(f"Hi,{user_name},What would you like to do?")
+            while True:
+                print('.'* 60)
+                short_code = input("Codes: CA - create an account or name of the site your want, DA- display the list of your accounts or sites, EX- exit the site \n").lower().strip()
+                print('.'* 60)
+                if short_code == "ca":
+                    print("Create new credentials")
+                    print('*' * 30)
+                    account_name = input("Account name/ Site name: ")
+                    account_username = input("Site User Name: ")
+                    print('.'* 60)
+                    password_option = input("Please choose between: (ep-enter existing password) or (gp-generate new password) \n").strip()
+                    print('.'* 60)
+                    while True:
+                        if password_option == "ep":
+                            account_password = input("Enter your password (minimum 8 characters): ")
+                            break
+                        elif password_option == "gp":
+                            account_password = generate_password()
+                            break
+                        else:
+                            print("Invalid option")
+                            break
+                    save_credential(create_credential(account_name,account_username,account_password))
+                    print('*' * 30)
+                    print(f"New created account: \n Account:{account_name}\n User Name:{account_username} \n Password: {account_password}")
+                    print('*' * 30)
+    
+                elif short_code == "da":
+                    if display_accounts():
+                        print("Here's your list of acoount(s): ")
+                        
+                        print('#' * 30)
+                        for account in display_accounts():
+                            print(f" Site:{account.account_name} \n User Name:{account_username}\n Password:{account_password}")
+                            print('_'* 30)
+                        print('#' * 30)
+                    else:
+                        print("No accounts yet")
+                        
+                elif short_code == "ex":
+                    print("See you again!!!!")
+                    break
+                else:
+                    print("Invalid Choice")
+                    
+                         
+                    
+#                break
+                
+        else:
+            print("Ooops!You dnt have an account. To proceed please create an account.")
+            
+#    break
+    
+
+
+
+if __name__ == '__main__':
+  main()
